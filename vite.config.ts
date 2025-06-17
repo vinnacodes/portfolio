eed19a863c84d0fda0dbccb985175aea1ea54eca
vinnacodes <vinnarasu2000@gmail.com>
2025-06-17 22:01:38 +0530

    final change

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: "/portfolio/", // Replace with your actual GitHub repo name
   plugins: [react()],
+  base: "/portfolio/", // <--- MUST match the repo name exactly
 });
